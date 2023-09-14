dfca84b6b7dc605622ca37666ddbdb4f84c96615
jiwon <pjw99032986@gmail.com>
2023-09-14 14:28:51 +0900

    1

--- app/src/main/java/com/example/aespa/ImageSelectedListener.kt
+++
@@ -1,8 +0,0 @@
-package com.example.aespa
-
-import android.net.Uri
-
-interface ImageSelectedListener {
-    fun onImageSelected(requestCode: Int)
-    fun onImageReceived(uri: Uri, position: Int)
-}
--- app/src/main/java/com/example/aespa/item.kt
+++
@@ -1,7 +0,0 @@
-package com.example.aespa
-
-class item {
-
-
-
-}
